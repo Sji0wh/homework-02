6d217b02d0ffd36794c5435df5b2c9eeb3212190
Mark Kozlov <mark@indicosys.com>
2023-10-27 14:49:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("ForestGreen");
+        System.out.println("MidnightBlue");
+        System.out.println("LightSteelBlue");
+        }
+    }
